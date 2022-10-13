44aabbf9fad2abf18370ce9683b56ec65ac625df
TheHumphrey <fernandob.delima@outlook.com>
2022-10-13 11:16:50 -0300

    feat: config global styles and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/globa'
+import { defaultTheme } from './styles/theme/default'
+
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>Hello World</h1>
-    </>
+    </ThemeProvider>
   )
 }
