69124672c469c48610ee8af81268009d15aa23cd
TheHumphrey <fernandob.delima@outlook.com>
2022-10-13 12:25:34 -0300

    feat: created transactions table

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,10 +1,38 @@
 import { Header, Summary } from '../../components'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './style'
 
 export const Transactions = () => {
   return (
     <>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Comida</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 1.000,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   )
 }
--- src/pages/Transactions/style.ts
+++ src/pages/Transactions/style.ts
@@ -1,1 +1,39 @@
 import styled from 'styled-components'
+
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  /* margin-top: 1.5rem; */
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${({ theme }) => theme['gray-700']};
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`
+
+interface PriceHighLightProps {
+  variant: 'income' | 'outcome'
+}
+
+export const PriceHighLight = styled.span<PriceHighLightProps>`
+  color: ${({ theme, variant }) =>
+    variant === 'income' ? theme['green-300'] : theme['red-300']};
+`
